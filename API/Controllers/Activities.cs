897020ade5038853294a18f9394a38cedb7e18af
Govindaraj Perumal <govindaraj.perumal@gmail.com>
2024-01-10 22:36:41 +0530

    End of session 2

---
+++ API/Controllers/Activities.cs
@@ -0,0 +1,31 @@
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Controllers
+{
+    public class Activities : BaseApiController
+    {
+        private readonly DataContext _context;
+        public Activities(DataContext context)
+        {
+            _context = context;
+
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Activity>>> GetActivities()
+        {
+            return await _context.Activities.ToListAsync();
+
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Activity>> GetActivity(Guid id)
+        {
+            return await _context.Activities.FindAsync(id);
+
+        }
+    }
+}
